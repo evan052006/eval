977854fd6c9b79779852df86dc5c23524f42c70d
Vane <88143635+evan052006@users.noreply.github.com>
2025-02-02 01:47:02 +0700

    made if structure

--- main.c
+++ main.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
+#include <math.h>
+
+// Add support for "log, sqrt, root maybe"
+static const int operators[256] = {
+    ['+'] = 1, ['-'] = 1, ['*'] = 1, ['/'] = 1,
+    ['%'] = 1, ['^'] = 1
+};
 
 typedef struct node {
    char data;
    struct node* next;
 } Node;
+
+double eval(char* s) {
+    while (*s != '/0') {
+        if (*s == ' ') { continue; }
+
+        if ('0' <= *s <= '9') {
+
+        }
+
+        if (operators[*s]) {
+
+        }
+
+        if (*s == '(') {
+
+        }
+
+        if (*s == ')') {
+            
+        }
+
+        s++;
+    }
+}
 
 int main() {
     return 0;
