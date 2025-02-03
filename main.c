a315c3d47e1a2cd1e8f8f002e7e1d03118b984e6
G30TR1X <168168157+G30TR1X@users.noreply.github.com>
2025-02-03 08:18:06 +0700

    Made createNode and freeNode function

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 // Add support for "log, sqrt, root maybe"
 static const int operators[256] = {
@@ -10,11 +11,11 @@ static const int operators[256] = {
 typedef struct node {
    char data;
    struct node* next;
-} Node;
+} node;
 
 double eval(char* s) {
     while (*s != '/0') {
-        if (*s == ' ') { continue; }
+        if (*s == ' ') continue;
 
         if ('0' <= *s <= '9') {
 
@@ -36,6 +37,50 @@ double eval(char* s) {
     }
 }
 
+node *createNode(char data) {
+    node *listNode = malloc(sizeof(node));
+
+    if (listNode == NULL)
+    {
+        return NULL;
+    }
+
+    listNode->data = data;
+    listNode->next = NULL;
+
+    return listNode;
+}
+
+void freeNode(node* head) {
+    while (head != NULL)
+    {
+        node *next = head->next;
+        free(head);
+        head = next;
+    }
+} 
+
 int main() {
+    char *equation = "Hello";
+    node *list = NULL;
+
+    for (int i = 0; i < 5; i++)
+    {
+        node *temp = createNode(equation[i]);
+
+        temp->next = list;
+        list = temp;
+    }
+
+    node *copy = list;
+    while (copy != NULL)
+    {
+        printf("%c", copy->data);
+        copy = copy->next;
+    }
+    printf("\n");
+
+
+    freeNode(list);
     return 0;
 }
